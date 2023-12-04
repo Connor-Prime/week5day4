57791798273691be8268946a988161ad6253c756
Connor-Prime <connorprime23@gmail.com>
2023-12-04 00:01:34 -0800

    first commit

---
+++ wk5day4homework.sql
@@ -0,0 +1,23 @@
+-- ALTER TABLE customer
+-- ADD Platinum_Member BOOLEAN
+
+UPDATE customer
+SET Platinum_Member = TRUE
+
+UPDATE customer
+SET Platinum_Member = TRUE
+WHERE customer_id IN
+	(SELECT  customer_id
+	FROM payment
+	GROUP BY customer_id
+	HAVING SUM(AMount) > 200)
+
+UPDATE customer
+SET Platinum_Member = FALSE
+WHERE customer_id NOT IN
+	(SELECT  customer_id
+	FROM payment
+	GROUP BY customer_id
+	HAVING SUM(AMount) > 200)
+	
+SELECT * FROM customer
